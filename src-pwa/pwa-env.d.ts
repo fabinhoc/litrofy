b987f06d532313778834e50818608452a52bc348
Fabio Cruz <fabinhocosta1202@gmail.com>
2026-04-29 19:57:30 -0300

    fix: Rewrite pwa-env.d.ts

--- src-pwa/pwa-env.d.ts
+++ src-pwa/pwa-env.d.ts
@@ -1,20 +1,19 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    SERVICE_WORKER_FILE: string;
-    PWA_FALLBACK_HTML: string;
-    PWA_SERVICE_WORKER_REGEX: string;
-  }
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly PROD: boolean;
+  readonly DEV: boolean;
+
+  // variáveis do Quasar PWA
+  readonly SERVICE_WORKER_FILE: string;
+  readonly PWA_FALLBACK_HTML: string;
+  readonly PWA_SERVICE_WORKER_REGEX: string;
+
+  // suas variáveis (exemplo)
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-  readonly hot?: {
-    accept(cb?: (mod: unknown) => void): void;
-    accept(dep: string, cb: (mod: unknown) => void): void;
-    accept(deps: string[], cb: (mods: unknown[]) => void): void;
-    dispose(cb: (data: unknown) => void): void;
-    decline(): void;
-    invalidate(): void;
-    on(event: string, cb: (...args: unknown[]) => void): void;
-  };
 }
