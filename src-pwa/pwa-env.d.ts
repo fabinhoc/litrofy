12d6e29d50430306930f84e6633f7c60fca78f91
Fabio Cruz <fabinhocosta1202@gmail.com>
2026-04-29 19:53:50 -0300

    fix: Change process env to import meta.

--- src-pwa/pwa-env.d.ts
+++ src-pwa/pwa-env.d.ts
@@ -1,9 +1,20 @@
-/* eslint-disable */
-
 declare namespace NodeJS {
   interface ProcessEnv {
     SERVICE_WORKER_FILE: string;
     PWA_FALLBACK_HTML: string;
     PWA_SERVICE_WORKER_REGEX: string;
   }
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+  readonly hot?: {
+    accept(cb?: (mod: unknown) => void): void;
+    accept(dep: string, cb: (mod: unknown) => void): void;
+    accept(deps: string[], cb: (mods: unknown[]) => void): void;
+    dispose(cb: (data: unknown) => void): void;
+    decline(): void;
+    invalidate(): void;
+    on(event: string, cb: (...args: unknown[]) => void): void;
+  };
 }
